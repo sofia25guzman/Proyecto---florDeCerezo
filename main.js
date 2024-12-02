204f5c578690f2c9be76bd3021786cc7deb39609
sofia <sofia.guzmanariza@hotmail.com>
2024-12-01 22:07:23 -0300

    se agrega function

--- main.js
+++ main.js
@@ -27,37 +27,44 @@ nombre = prompt("Ingrese su nombre: ");
 apellido = prompt("Ingrese su apellido: ");
     alert("Gracias por registrase en nuestra página"+ " "+nombre+" "+apellido);
 
-do {
-    producto = prompt("Que producto desea comprar?\n vestidos\n pantalones\n zapatos\n carteras").toLowerCase();
-    cantidad = parseInt(prompt("Cuantos desea llevar?"));
-
-while (cantidad <= 0){
-    alert("Cantidad inválidad");
-    cantidad = parseInt(prompt("Cuantos desea llevar?"));
-};
-switch(producto){
-    case "vestidos":
-        precio = 20000;
-        break;
-    case "pantalones":
-        precio = 40000;
-        break;
-    case "zapatos":
-        precio = 60000;
-        break;
-    case "carteras":
-        precio = 20000;
-        break;
-    default:
-        alert("Producto fuera de catálogo")
-};
-precioTotal += precio * cantidad;
-cantidadTotal += cantidad;
-
-seguirComprando = confirm("Desea seguir comprando?");
-}while (seguirComprando);
-
-alert("La cantidad de productos es: "+cantidadTotal+" y el precio total es: "+precioTotal);
-alert("Gracias por comprar en Flor de cerezo");
+    function calcularPrecio(producto, cantidad) {
+        let precio;
+        switch (producto) {
+            case "vestidos":
+                precio = 20000;
+                break;
+            case "pantalones":
+                precio = 40000;
+                break;
+            case "zapatos":
+                precio = 60000;
+                break;
+            case "carteras":
+                precio = 20000;
+                break;
+            default:
+                alert("Producto fuera de catálogo");
+                return 0; 
+        }
+        return precio * cantidad; 
+    }
+    
+    do {
+        let producto = prompt("Elija un producto: vestidos, pantalones, zapatos, carteras").toLowerCase();
+        let cantidad = parseInt(prompt("¿Cuántos productos desea comprar?"));
+    
+        let totalProducto = calcularPrecio(producto, cantidad); 
+    
+        if (totalProducto > 0) {
+            precioTotal += totalProducto; 
+            cantidadTotal += cantidad; 
+        }
+    
+        seguirComprando = confirm("¿Desea seguir comprando?");
+    } while (seguirComprando);
+    
+    alert("La cantidad de productos es: " + cantidadTotal + " y el precio total es: " + precioTotal);
+    alert("Gracias por comprar en Flor de cerezo");
+    
 
 
